b34b1880a642e993edd4c1ce0dad4bfca34729b3
Petr Tvaroha <ptvaroha@gmail.com>
2021-02-04 10:49:22 +0100

    Update xml-parse.php

--- xml-parse.php
+++ xml-parse.php
@@ -4,7 +4,7 @@ $url = 'https://example.com/rss.php';
 $dom = new DOMDocument();
 libxml_use_internal_errors(true);       // supressing errors
 $dom->recover = true;                   // Enables recovery mode, i.e. trying to parse non-well formed documents. @see https://www.php.net/manual/en/class.domdocument.php#domdocument.props.recover
-$dom->loadXML(file_get_contents($url));
+$dom->load($url);
 $xml = $dom->saveXML();
 $parsedXML = simplexml_load_string($xml);
 
